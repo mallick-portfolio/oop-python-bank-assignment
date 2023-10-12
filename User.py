91ee01f4b196e24d102766e70dad69923ce32525
mallick-portfolio <tamal1714019@gmail.com>
2023-10-12 22:01:40 +0600

    added User class

--- User.py
+++ User.py
@@ -1,3 +1,18 @@
+import uuid
+
+
 class User:
-  def __init__(self) -> None:
-    pass
+    # his/her name, email, address and account
+    def __init__(self, name, email, password, address, account_type) -> None:
+        self.name = name
+        self.email = email
+        self.password = password
+        self.address = address
+        self.account_type = account_type
+        self.account_no = uuid.uuid4()
+        self.balance = 0
+        pass
+
+
+tamal = User("tamal", 'tamal@gmail.com', 'dhaka', "saving")
+print(tamal.account_no)
